79d0a66bde62b60da71aeade8869cd51a1c748a6
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:21:33 +0100

    creating ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var lastName= "Doe";
+    var lastNameLength=lastName.length;
+
+
+
+
+
+    //Only change code below this line
+
+
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
