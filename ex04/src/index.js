b2e0cbcda990249d418e1d57fb7aaee9483a3e9e
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:04:07 +0100

    creating ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+   
+    
+    //Only change code below this line
+
+   var myString= "This is the first string " + "This is the second string";
+   
+   
+
+
+    //Only change code below this line
+
+
+return myString;
+}
+
+console.log(main()); 
+module.exports=main;
