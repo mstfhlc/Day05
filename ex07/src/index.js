3180180199a76af7ed2a6bd2b9914cc32b3602c8
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:16:28 +0100

    creating ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+
+
+    //Only change code below this line
+    var someAdjective = "the best!";
+    var myString = "Mistral is " ;
+    myString+=someAdjective;
+
+
+
+
+
+    //Only change code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
