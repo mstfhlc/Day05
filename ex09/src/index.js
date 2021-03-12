d509b703497e41e4f95a8678370f3e69a339a9af
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:35:25 +0100

    creating ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var lastName="Doe";
+    var firstLetterOfLastName=" ";
+    firstLetterOfLastName=lastName[0];
+
+
+
+
+    //Only change code below this line
+
+
+    return firstLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
