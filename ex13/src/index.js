7aad275bb5dc3fac163440753e1a2ee57fb3dbf8
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:53:35 +0100

    creating ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var lastName= "Bezos";
+    var thirdToLastLetterOfLastName=lastName[lastName.length-3];
+
+
+
+
+
+    //Only change code below this line
+
+
+    return thirdToLastLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
