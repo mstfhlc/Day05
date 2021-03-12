5070ab58b4969841f87316962d6c8a93437ea2d9
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 22:11:44 +0100

    creating ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+
+
+    //Only change code below this line
+    var myNoun= "dog ";
+    var myVerb=" ran ";
+    var myAdjective=" big ";
+    var myAdverb=" quickly";
+    var wordBlanks=myNoun+"_____"+myAdjective+"____"+myVerb+"_____"+myAdverb+".";
+   
+
+
+
+
+
+    //Only change code below this line
+
+
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
