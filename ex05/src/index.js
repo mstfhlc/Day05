e07ec04539e9eee0e01a0bc8f1578a96ff1c983e
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:08:18 +0100

    creating ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+   
+    
+    //Only change code below this line
+
+   var myString= "Concatenation is the process od appending";
+   myString +=" one string to the end of another string";
+   
+   
+
+
+    //Only change code below this line
+
+
+return myString;
+}
+
+console.log(main()); 
+module.exports=main;
