161908cdb8678bcadaa6ce1a74df953c570cf501
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:49:28 +0100

    creating ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var lastName= "Tesla";
+    var lastLetterOfLastName=lastName[lastName.length-1];
+
+
+
+
+
+    //Only change code below this line
+
+
+    return lastLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
