405fe93bf97d7e544e2c41e0e06c1d21741769b3
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:13:04 +0100

    creating ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var myName = "Mustafa"
+    var myString = "Hello, my name is " + myName + " and I am well";
+
+
+
+
+
+    //Only change code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
