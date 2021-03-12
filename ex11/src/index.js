b825930995482097a9eba379321f97ba521903f6
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:45:26 +0100

    creating ex11

---
+++ ex11/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var lastName= "Tesla";
+    var thirdLetterOfLastName=lastName[2];
+
+
+
+
+
+    //Only change code below this line
+
+
+    return thirdLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
