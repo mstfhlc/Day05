032c51d915c99ecb82066cc1e2ad402279d22b77
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 20:47:34 +0100

    creating ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+   
+    
+    //Only change code below this line
+
+   var myLink= '<a href="http://www.example.com"target="_blank"> Link </a>'
+   
+   
+
+
+    //Only change code below this line
+
+
+return myLink;
+}
+
+console.log(main()); 
+
+
+module.exports=main;
