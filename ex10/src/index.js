10f2b6c26f53188f882a0c679238484d763d56d1
mstfhlc <mustafahalici30@gmail.com>
2021-03-12 21:42:36 +0100

    creating ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+
+
+    //Only change code below this line
+    var myString="Jello World";
+    myString="Hello World";
+    
+
+
+
+
+    //Only change code below this line
+
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
